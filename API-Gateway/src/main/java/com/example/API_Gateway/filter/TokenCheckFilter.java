f95db38eb582400eccedb0089e0919a5b40ba41f
junhyeonpark <j0303p@gmail.com>
2025-04-15 10:46:04 +0900

    refactor: api-gw환경변수로 빼기 및 의존성 분리

--- API-Gateway/src/main/java/com/example/API_Gateway/filter/TokenCheckFilter.java
+++ API-Gateway/src/main/java/com/example/API_Gateway/filter/TokenCheckFilter.java
@@ -3,7 +3,6 @@ package com.example.API_Gateway.filter;
 
 import com.example.API_Gateway.error.CustomException;
 import com.example.API_Gateway.error.ErrorCode;
-import com.example.API_Gateway.util.CryptoUtil;
 import com.example.API_Gateway.util.JWEUtil;
 import com.example.API_Gateway.util.JWTUtil;
 import lombok.extern.slf4j.Slf4j;
@@ -24,16 +23,14 @@ import java.util.Optional;
 public class TokenCheckFilter implements GlobalFilter, Ordered {
 
     private final JWTUtil jwtUtil;
-    private final CryptoUtil cryptoUtil;
-    private  final List<String> permitUrl;
-    private  final List<String> needIdUrl;
+    private final List<String> permitUrl;
+    private final List<String> needIdUrl;
     private final JWEUtil jweUtil;
 
-    public TokenCheckFilter(JWTUtil jwtUtil, List<String> permitUrl, List<String> needIdUrl,CryptoUtil cryptoUtil,JWEUtil jweUtil) {
+    public TokenCheckFilter(JWTUtil jwtUtil, List<String> permitUrl, List<String> needIdUrl, JWEUtil jweUtil) {
         this.jwtUtil = jwtUtil;
         this.permitUrl = permitUrl;
         this.needIdUrl = needIdUrl;
-        this.cryptoUtil = cryptoUtil;
         this.jweUtil = jweUtil;
     }
 
@@ -44,7 +41,7 @@ public class TokenCheckFilter implements GlobalFilter, Ordered {
         String path = request.getURI().getPath();
         log.info("TokenCheckFilter path: {}", path);
 
-        if(permitUrl.stream().anyMatch(path::contains)) {
+        if (permitUrl.stream().anyMatch(path::contains)) {
             log.info("인증 미필요 url통과");
             return chain.filter(exchange);
         }
@@ -54,9 +51,9 @@ public class TokenCheckFilter implements GlobalFilter, Ordered {
                 .orElseThrow(() -> new CustomException(ErrorCode.UNAUTHORIZED))
                 .replace("Bearer ", "");
 
-        Map<String, Object> payload = jwtUtil.validateToken(tokenStr,path);
+        Map<String, Object> payload = jwtUtil.validateToken(tokenStr, path);
 
-        if(!needIdUrl.stream().anyMatch(path::contains)) {
+        if (!needIdUrl.stream().anyMatch(path::contains)) {
             log.info("id 필요 리스트 통과");
             return chain.filter(exchange);
         }
@@ -64,7 +61,7 @@ public class TokenCheckFilter implements GlobalFilter, Ordered {
         // 숫자인지 한번 확인 필요할 수도 //필요한 것만 복호화 시키기
 //        Integer id = cryptoUtil.decrypt((String) payload.get("payload"));
         System.out.println("여기까지");
-        Integer id = jweUtil.getIdFromDecrpytJWE((String) payload.get("payload"));
+        Integer id = jweUtil.getIdFromDecryptJWE((String) payload.get("payload"));
         log.info("id: {}", id);
 
         ServerHttpRequest mutatedRequest = request.mutate()
--- API-Gateway/src/main/java/com/example/API_Gateway/util/JWEUtil.java
+++ API-Gateway/src/main/java/com/example/API_Gateway/util/JWEUtil.java
@@ -4,88 +4,43 @@ import com.example.API_Gateway.error.CustomException;
 import com.example.API_Gateway.error.ErrorCode;
 import com.nimbusds.jose.JOSEException;
 import com.nimbusds.jose.JWEObject;
-import com.nimbusds.jose.Payload;
 import com.nimbusds.jose.crypto.DirectDecrypter;
-import com.nimbusds.jose.crypto.DirectEncrypter;
-import com.nimbusds.jose.crypto.RSADecrypter;
-import io.jsonwebtoken.security.Keys;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
-import javax.crypto.SecretKeyFactory;
-import javax.crypto.spec.PBEKeySpec;
-import javax.crypto.spec.SecretKeySpec;
-import java.nio.charset.StandardCharsets;
-import java.security.Key;
-import java.security.KeyFactory;
-import java.security.PrivateKey;
-import java.security.spec.KeySpec;
 import java.text.ParseException;
 import java.util.Map;
 
 @Component
-//@RequiredArgsConstructor
 @Slf4j
 public class JWEUtil {
     private final SecretKey key;
 
-    public JWEUtil() {
-        byte[] keyBytes = "your-predefined-secret-key".getBytes(StandardCharsets.UTF_8);
-        key = generateKeyFromPassword("doif023nsdfoasdjfkosadfksajd",keyBytes);
+    public JWEUtil(@Qualifier("jweSecretKey") SecretKey key) {
+        this.key = key;
     }
 
-    public static SecretKey generateKeyFromPassword(String password,byte[] salt) {
-        try {
-            // PBEKeySpec을 이용해 password와 salt로 key를 생성
-            KeySpec spec = new PBEKeySpec(password.toCharArray(), salt, 65536, 256);
+    public Integer getIdFromDecryptJWE(String jwt) {
+        Map<String, Object> claims = decryptWithNimbus(jwt);
+        Object payload = claims.get("payload");
 
-            // SecretKeyFactory를 사용하여 키 생성
-            SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
-            byte[] keyBytes = factory.generateSecret(spec).getEncoded();  // 비밀 키 생성
-
-            // 생성된 바이트 배열로 SecretKey 생성
-            return new SecretKeySpec(keyBytes, "AES");
-        } catch (Exception e) {
-            throw new RuntimeException("키 생성 중 오류 발생", e);
+        if (!(payload instanceof Number)) {
+            throw new CustomException(ErrorCode.INVALID_DECRYPTION);
         }
-    }
-
-
-    public Integer getIdFromDecrpytJWE(String jwt) {
-        try {
-            Map<String, Object> claims = decryptWithNimbus(jwt);
-            Long idLong = (Long) claims.get("payload");
-            Integer id = idLong.intValue();
-            return id;
-        }catch (Exception e) {
-            log.error(e.getMessage());
-            throw  new CustomException(ErrorCode.INVALID_DECRYPTION);
-        }
+        int id = ((Number) payload).intValue();
+        return id;
     }
 
     private Map<String, Object> decryptWithNimbus(String jweToken) {
         try {
-            // 1. JWE 토큰 파싱
             JWEObject jweObject = JWEObject.parse(jweToken);
-
-            // 2. RSA 개인키로 복호화
             jweObject.decrypt(new DirectDecrypter(key));
-            Payload payload = jweObject.getPayload();
-            // 3. 페이로드 추출 및 Map 변환
-            Map<String, Object> claims =  payload.toJSONObject();
-            return claims;
-        } catch (ParseException e) {
-            log.error("JWE 파싱 실패: {}", e.getMessage());
-//            throw new IllegalArgumentException("잘못된 JWE 형식입니다.", e);
-            throw  new CustomException(ErrorCode.INVALID_DECRYPTION);
-        } catch (JOSEException e) {
+            return jweObject.getPayload().toJSONObject();
+        } catch (ParseException | JOSEException e) {
             log.error("JWE 복호화 실패: {}", e.getMessage());
-            throw  new CustomException(ErrorCode.INVALID_DECRYPTION);
-
-        } catch (Exception e) {
-            log.error("JWE 처리 중 알 수 없는 오류: {}", e.getMessage());
-            throw  new CustomException(ErrorCode.INVALID_DECRYPTION);
+            throw new CustomException(ErrorCode.INVALID_DECRYPTION);
         }
     }
 }
