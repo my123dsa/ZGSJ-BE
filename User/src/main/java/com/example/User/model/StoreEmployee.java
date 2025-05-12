38e885e8d843080810afafbfda0ef0e2422e9957
junhyeonpark <j0303p@gmail.com>
2025-05-12 16:21:09 +0900

    feat: cdc 데이터 버전 위한 버전 추가

--- User/src/main/java/com/example/User/model/StoreEmployee.java
+++ User/src/main/java/com/example/User/model/StoreEmployee.java
@@ -18,7 +18,7 @@ import java.time.LocalDate;
 )
 @Getter
 @NoArgsConstructor
-public class StoreEmployee {
+public class StoreEmployee extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
