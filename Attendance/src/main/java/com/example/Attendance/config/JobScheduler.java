eaf341cc7d4ac77d15daaff463da16c29df74aef
junhyeonpark <j0303p@gmail.com>
2025-07-08 09:53:17 +0900

    feat: api-gw yaml 수정

--- Attendance/src/main/java/com/example/Attendance/config/JobScheduler.java
+++ Attendance/src/main/java/com/example/Attendance/config/JobScheduler.java
@@ -26,8 +26,8 @@ public class JobScheduler {
     private final JobLauncher jobLauncher;
     private final Job attendanceJob; //job이 하나라 자동으로 해당 job주입
 
-    @Scheduled(cron = "0 * * * * *")  // 매일 새벽 4시 실행
-//@Scheduled(cron = "0 0 4 * * *")
+//    @Scheduled(cron = "0 * * * * *")  // 매일 새벽 4시 실행
+@Scheduled(cron = "0 0 4 * * *")
     public void runJob() {
         String dateParam = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
 
